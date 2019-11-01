0296e1edac43e6344807ba1d695ab0b3cb3a032a
xhr-catcher <1240960040@qq.com>
2019-11-01 11:38:25 +0800

    加入一个python文件

--- website/xhr_test.py
+++ website/xhr_test.py
@@ -1,9 +1,4 @@
 import torch
-import tensorflow as tf
 x = torch.rand(5, 3)
 print(x)
 print(torch.cuda.is_available())
-
-hello = tf.constant("Hello!TensorFlow")
-sess = tf.Session()
-print(sess.run(hello))
